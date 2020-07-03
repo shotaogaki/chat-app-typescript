5187923b265aabf50c4d21d6ad6a7d72dc43ce8c
shotaogaki <gomazochannel@gmail.com>
2020-07-03 11:47:02 +0900

    edit config and gitignore

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,19 +1,10 @@
-// require('dotenv').config();
-
-// export default {
-//     apiKey: process.env.apiKey,
-//     authDomain: process.env.authDomain,
-//     databaseURL: process.env.databaseURL,
-//     projectId: process.env.projectId,
-//     storageBucket: process.env.storageBucket,
-//     messagingSenderId: process.env.messagingSenderId
-//   };
+require('dotenv').config();
 
 export default {
-     apiKey: "AIzaSyDyN0GKq1maXn8WgeTvAdVc-PQOC73KDo0",
-     authDomain: "chat-app-typescript.firebaseapp.com",
-     databaseURL: "https://chat-app-typescript.firebaseio.com",
-     projectId: "chat-app-typescript",
-     storageBucket: "chat-app-typescript.appspot.com",
-     messagingSenderId: "629108362881",
- }
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+  };
